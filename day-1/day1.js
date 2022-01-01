d01be582f3bfcbc68eb90e1682b9ea1489777301
pSapien <prateekthapa26@gmail.com>
2022-01-01 19:16:09 +0545

    remove extra loop for day 1 solution

--- day-1/day1.js
+++ day-1/day1.js
@@ -1,30 +1,18 @@
-const fs = require('fs').promises;
-
+const fs = require("fs").promises;
 
 async function main() {
-  const fileBlob = await fs.readFile('./day1-data.txt', 'utf8');
-  const arrayData = fileBlob.split('\n');
+  const fileBlob = await fs.readFile("./day1-data.txt", "utf8");
+  const arr = fileBlob.split("\n");
 
-  console.log(increasedCountFinder(arrayData, 3))
+  let increasedCount = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 3] === undefined) break;
+
+    if (Number(arr[i + 3]) > Number(arr[i])) increasedCount++;
+  }
+
+  console.log(increasedCount);
 }
 
 main(); // 1311
-
-const sum = (arr) => arr.reduce((acc, curr) => Number(acc) + Number(curr), 0);
-
-function increasedCountFinder(arr, chunkSize) {
-  let increasedCount = 0;
-
-  for (let head = 0; head < arr.length; head++) {
-    const tail = head + chunkSize;
-
-    if (arr[tail] === undefined) break;
-
-    const firtSum = sum(arr.slice(head, tail));
-    const secondSum = sum(arr.slice(head + 1, tail + 1));
-
-    if (secondSum > firtSum) increasedCount++;
-  }
-
-  return increasedCount
-}
